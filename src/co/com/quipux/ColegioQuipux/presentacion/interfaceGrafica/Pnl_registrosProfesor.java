c26f930fdc00078a766136e5fb7505834b6b07e7
Daniel Garcia <daniigarciagil321@gmail.com>
2018-09-22 11:46:46 -0500

    vamos a pasar

--- src/co/com/quipux/ColegioQuipux/presentacion/interfaceGrafica/Pnl_registrosProfesor.java
+++ src/co/com/quipux/ColegioQuipux/presentacion/interfaceGrafica/Pnl_registrosProfesor.java
@@ -27,7 +27,7 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author Sebas
+ * vamo a pasa esta joda manito queloquemaniga
  */
 public class Pnl_registrosProfesor extends JPanel implements ActionListener{
     protected Control_Instancias pp;
